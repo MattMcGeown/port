be90a2366b4cddd33aed5844dca0e0f04fed03e3
Matt McGeown <matt_live@live.co.uk>
2022-10-03 23:07:02 +0100

    fix esc err

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -23,8 +23,10 @@ const Contact = (props: Props) => {
 
       <div className="flex flex-col space-y-10">
         <h4 className="text-4xl font-semibold text-center">
-          I've got just what you need,{' '}
-          <span className="decoration-[#F7AB0A]/50 underline">Let's talk.</span>
+          {"I've got just what you need"},{' '}
+          <span className="decoration-[#F7AB0A]/50 underline">
+            {"Let's talk."}
+          </span>
         </h4>
         <div className="space-y-10">
           <div className="flex items-center justify-center space-x-5">
